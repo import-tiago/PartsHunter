78c504157946b9636654ed4a6259b196e231b367
Tiago Silva <tiagodepaulasilva@gmail.com>
2021-03-24 17:30:19 -0300

    Added popup alert to register parts with same description

--- Mobile App/partshunter/lib/screens/register_screen.dart
+++ Mobile App/partshunter/lib/screens/register_screen.dart
@@ -11,6 +11,7 @@ import 'package:partshunter/screens/search_screen.dart';
 import 'dart:convert';
 
 import 'package:partshunter/stores/store.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 //Screens
 
@@ -44,12 +45,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
     setState(() => Current_BottomNavigation_Index = index);
     switch (index) {
       case 0:
-      store.Set_Hardware_Device(-1);
+        store.Set_Hardware_Device(-1);
         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SearchScreen()));
         break;
 
       case 2:
-      store.Set_Hardware_Device(-1);
+        store.Set_Hardware_Device(-1);
         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => KeypadScreen()));
         break;
     }
@@ -121,7 +122,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               height: 40,
               margin: EdgeInsets.only(right: 10, left: 10, top: 20),
               child: TextField(
-                keyboardType: TextInputType.number,                
+                keyboardType: TextInputType.number,
                 onChanged: (input) => store.Register_Drawer = input,
                 textAlign: TextAlign.left,
                 decoration: InputDecoration(
@@ -146,8 +147,49 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       Last_Register_Drawer = store.Register_Drawer;
                     } else
                       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Fail")));
-                  } else
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Fail! Some Description and/or Drawer")));
+                  } else {
+                              Alert(
+                      context: context,
+                      type: AlertType.warning,
+                      title: "ITEM ALREADY ADDED",
+                      desc: "Some Description and/or Drawer",
+                      buttons: [
+                        DialogButton(
+                            child: Container(
+                              height: 40,
+                              width: 90,
+                              child: Center(
+                                child: Text(
+                                  'CONTINUE',
+                                  style: TextStyle(fontSize: 17, color: Colors.white),
+                                ),
+                              ),
+                              decoration: new BoxDecoration(
+                                borderRadius: new BorderRadius.all(new Radius.circular(5.0)),
+                                color: Color.fromRGBO(41, 55, 109, 1.0),
+                              ),
+                            ),
+                            onPressed: () {
+                           Run_Duplicate_Part_Decision();
+        
+
+                              Navigator.of(context, rootNavigator: true).pop();
+                            },
+                            color: Colors.transparent),
+                        DialogButton(
+                          child: Text(
+                            'ABORT',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromRGBO(41, 55, 109, 1.0),
+                            ),
+                          ),
+                          onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
+                          color: Colors.transparent,
+                        )
+                      ],
+                    ).show();
+                  }
                 },
                 child: Text(
                   'SAVE',
@@ -209,5 +251,66 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
           )),
     );
+  }
+
+  bool showAlertDialog() {
+    bool _return;
+    Alert(
+      context: context,
+      type: AlertType.warning,
+      title: "ALREADY ADDED PART",
+      desc: "Some Description and/or Drawer",
+      buttons: [
+        DialogButton(
+            color: Colors.transparent,
+            child: TextButton(
+                onPressed: () {
+                 
+                },
+                child: Text(
+                  'CONTINUE',
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(41, 55, 109, 1.0)),
+                    side: MaterialStateProperty.all(BorderSide(width: 2, color: Color.fromRGBO(41, 55, 109, 1.0))),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 5, horizontal: 10)),
+                    textStyle: MaterialStateProperty.all(TextStyle(fontSize: 30)))),
+            
+            onPressed: () {
+
+              //Run_Duplicate_Part_Decision();
+
+              _return = true;
+              Navigator.of(context, rootNavigator: true).pop();
+            }),
+        DialogButton(
+            color: Colors.transparent,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'ABORT',
+                style: TextStyle(fontSize: 16, color: Color.fromRGBO(41, 55, 109, 1.0)),
+              ),
+            ),
+            onPressed: () {
+              _return = false;
+              Navigator.pop(context);//Navigator.of(context, rootNavigator: true).pop();
+            })
+      ],
+    ).show();
+    return _return;
+  }
+
+  Future<void> Run_Duplicate_Part_Decision() async {
+    if (await store.createData(store.Register_Category, store.Register_Description, store.Register_Drawer) == true) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Success")));
+
+      Last_Register_Category = store.Register_Category;
+      Last_Register_Description = store.Register_Description;
+      Last_Register_Drawer = store.Register_Drawer;
+    } else
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Fail")));
   }
 }
