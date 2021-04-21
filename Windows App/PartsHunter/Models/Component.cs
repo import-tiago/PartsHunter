d61464b0f740dec7aa89eb4d14164926c05690c7
caiodepaulasilva <caiodepaula.silva@gmail.com>
2021-04-21 19:53:57 -0300

    including: models (led & component)

---
+++ Windows App/PartsHunter/Models/Component.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsHunter.Models
+{
+    public class Component
+    {
+        public string Category { get; set; }
+        public string Drawer { get; set; }
+        public string Description { get; set; }
+    }
+}
