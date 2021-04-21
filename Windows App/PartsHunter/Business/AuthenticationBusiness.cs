66330741e2e7999c77a81f8ee0d9330b1f64012a
caiodepaulasilva <caiodepaula.silva@gmail.com>
2021-04-21 19:53:01 -0300

    including: authenticationBusiness

---
+++ Windows App/PartsHunter/Business/AuthenticationBusiness.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using PartsHunter.Business.Interfaces;
+using PartsHunter.Repositories.Interfaces;
+
+namespace PartsHunter.Business
+{
+    public class AuthenticationBusiness : IAuthenticationBusiness
+    {
+        private readonly IFirebaseRepositorie _firebaseRepositorie;
+
+        public AuthenticationBusiness(IFirebaseRepositorie firebaseRepositorie)
+        {
+            _firebaseRepositorie = firebaseRepositorie;
+        }
+
+        public void SaveSecrets(string url, string key)
+        {
+            if (_firebaseRepositorie.Autheticate())
+            {
+                Hashtable variables = new Hashtable {
+                    { "URL", url },
+                    { "KEY", key }
+                };
+
+                FileStream fs = new FileStream("Firebase_Secrets.dat", FileMode.Create);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                try
+                {
+                    formatter.Serialize(fs, variables);
+                    //MessageBox.Show("This will cloase, reload the application!");
+                    fs.Close();
+
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                    throw;
+                }
+                finally
+                {
+                    fs.Close();
+                }
+            }
+        }
+
+        public void LoadSecrets()
+        {
+            FileStream fs = new FileStream("Firebase_Secrets.dat", FileMode.OpenOrCreate);
+            
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                Hashtable variables = (Hashtable)formatter.Deserialize(fs);
+                string firebase_Database_URL = (string)variables["URL"];
+                string firebase_Database_KEY = (string)variables["KEY"];
+            }
+            catch 
+            {
+
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+    }
+}
---
+++ Windows App/PartsHunter/Business/Interfaces/IAuthenticationBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsHunter.Business.Interfaces
+{
+    public interface IAuthenticationBusiness
+    {
+        void SaveSecrets(string url, string key);
+
+        void LoadSecrets();
+    }
+}
